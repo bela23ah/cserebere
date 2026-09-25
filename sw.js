49bfcaa84e7078217e8fc513089802d5fb06b9f1
bela23ah <gyorgy.harkai@gmail.com>
2026-09-25 23:07:46 +0200

    Update service worker version to v4.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // =========================================================================
-// Lutra Album Cserebere - Service Worker (sw.js v4.0)
+// Lutra Album Cserebere - Service Worker (sw.js v4.1)
 // =========================================================================
 
 const CACHE_NAME = 'lutra-csere-v2.0';
